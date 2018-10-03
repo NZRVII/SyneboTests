3aaae837149d7962bd1bb7f49f63afb09b952f3d
Igor Nazarov <igorexa@ukr.net>
2018-10-04 00:35:56 +0300

    ready again :)

--- src/Matches.java
+++ src/Matches.java
@@ -10,7 +10,7 @@ public class Matches {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         int sqares = Integer.parseInt(in.readLine());
 
-        if (1<= sqares && sqares <= Math.pow(10, 9)) {
+        if (1<= sqares && sqares <= 1000000000) {
             double x = Math.floor(Math.sqrt(sqares));
             double y = Math.ceil((sqares / Math.sqrt(sqares)));
             System.out.println((int)(x + y + (2 * sqares)));
