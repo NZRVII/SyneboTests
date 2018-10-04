332a96ff49f847b52c3a9b6b4629fee5e2f03a86
Igor Nazarov <igorexa@ukr.net>
2018-10-04 18:16:17 +0300

    ispravil factorial

--- src/TheFactorial.java
+++ src/TheFactorial.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 
 /**
  * Created by Shadowdancer on 03.10.2018.
@@ -8,19 +9,12 @@ import java.io.InputStreamReader;
 public class TheFactorial {
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(in.readLine());
-
-        if (1 <= n && n <= 2000) {
+        BigInteger n = new BigInteger(in.readLine());
         int result = 0;
         do {
-            n /= (++result);
+            n = n.divide(new BigInteger(Integer.toString(++result)));
         }
-        while (n > 1);
-
+        while (n.compareTo(BigInteger.ONE) == 1);
         System.out.println(result);
-        }else {
-            System.out.println("введенное число не соответствует условиям задания");
-        }
-
     }
 }
